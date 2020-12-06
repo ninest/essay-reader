457ef5e56774ba174f4fb14f640b70fc188f2fc4
ninest <parth.kabra@gmail.com>
2020-12-06 19:24:49 +0800

    First commit

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import "./styles/root.scss";
+
 createApp(App)
   .use(router)
   .mount("#app");
---
+++ src/pages/Index.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1 class="title">Essay Reader</h1>
+  <form @submit.prevent="read()">
+    <textarea
+      placeholder="Paste your essay here ..."
+      v-model="essay"
+      id="essay-space"
+    ></textarea>
+
+    <button type="submit" class="button">Read</button>
+  </form>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const essay = ref<string>('');
+
+function read() {
+  console.log(essay.value);
+}
+</script>
