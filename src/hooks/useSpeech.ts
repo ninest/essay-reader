4fa592d8fe76ea82c58b4e525c5583d61d780246
ninest <parth.kabra@gmail.com>
2020-12-09 23:47:54 +0800

    Fixed voices
    Also added more bugs to fix later

--- src/hooks/useSpeech.ts
+++ src/hooks/useSpeech.ts
@@ -2,35 +2,59 @@ import { reactive, ref } from 'vue';
 
 let voices = ref<SpeechSynthesisVoice[]>([]);
 
-const options = reactive({
-  voice: voices.value[0],
-  volume: 0.5, // 0 to 1
-  rate: 5, // 0.1 to 10
-  pitch: 1, // 1 or 2
-  text: ''
-});
-
+/*
+Seems to be required on some browsers to populate the the voices
+*/
+voices.value = window.speechSynthesis.getVoices();
+// speechSynthesis.speak(new SpeechSynthesisUtterance());
 speechSynthesis.onvoiceschanged = function() {
+  console.log('getting voices');
   voices.value = window.speechSynthesis.getVoices();
   options.voice = voices.value[0];
 };
+
+const options = reactive({
+  voice: voices.value[0],
+  text: ''
+  // Other options: volume (0,1), rate (0.1,10), pitch (1,2)
+});
+
+const state = reactive({
+  playing: false
+});
 
 export default function useSpeech() {
   const utterance = new SpeechSynthesisUtterance();
 
   function say() {
     utterance.voice = options.voice;
-    utterance.volume = options.volume;
-    utterance.rate = options.rate;
-    utterance.pitch = options.pitch;
     utterance.text = options.text;
 
     speechSynthesis.speak(utterance);
   }
 
-  return {
-    voices,
-    options,
-    say
-  };
+  function stop() {
+    if (speechSynthesis.speaking) {
+      speechSynthesis.cancel();
+      speechSynthesis.cancel();
+      speechSynthesis.cancel();
+      speechSynthesis.cancel();
+      speechSynthesis.cancel();
+      speechSynthesis.cancel();
+      speechSynthesis.cancel();
+      console.log('STOPPED!');
+    } else {
+      console.log('NOT SPEAKING!');
+    }
+  }
+
+  // utterance.onstart = utterance.onresume = function() {
+  //   state.playing = true;
+  // };
+
+  // utterance.onend = utterance.onpause = function() {
+  //   state.playing = false;
+  // };
+
+  return { state, voices, options, say, stop };
 }
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,33 @@
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import useSpeech from '@/hooks/useSpeech';
+
+export default defineComponent({
+  name: 'Index',
+  setup() {
+    const { state, voices, options, say, stop } = useSpeech();
+
+    return {
+      state,
+      voices,
+      options,
+      say,
+      stop
+    };
+  }
+});
+</script>
+
 <template>
   <!-- <h1>test</h1> -->
   <h1 class="title">Essay Reader</h1>
-  <form @submit.prevent="read()">
+  <form @submit.prevent="say()">
     <ul class="form">
       <!-- DEBUGGING: -->
-      <li class="form-group">
-        <pre>{{ options }}</pre>
-      </li>
+      <!-- <li class="form-group">
+        <h2>debugging:</h2>
+        <pre>{{ voices }}</pre>
+      </li> -->
 
       <li class="form-group form-group-essay">
         <textarea
@@ -15,77 +36,26 @@
           id="essay-space"
         ></textarea>
       </li>
-      <li class="form-group form-group-sliders">
-        <div class="slider-group">
-          <label for="volume">Volume</label>
-          <input
-            v-model.number="options.volume"
-            type="range"
-            min="0"
-            max="1"
-            step="0.1"
-            name="volume"
-            id="volume"
-          />
-        </div>
 
-        <div class="slider-group">
-          <label for="rate">Rate</label>
-          <input
-            v-model.number="options.rate"
-            type="range"
-            min="0"
-            max="10"
-            step="0.1"
-            name="rate"
-            id="rate"
-          />
-        </div>
-      </li>
       <li class="form-group form-group-options">
         <select v-model="options.voice">
+          <option disabled value="">Choose voice</option>
           <option v-for="voice in voices" :key="voice" :value="voice">
             {{ voice.name }}
           </option>
         </select>
 
-        <button type="submit" class="button">Read</button>
+        <div class="button-group">
+          <button
+            v-if="state.playing"
+            @click="stop()"
+            class="button button-danger button-secondary"
+          >
+            Stop
+          </button>
+          <button type="submit" class="button button-main">Read</button>
+        </div>
       </li>
     </ul>
   </form>
 </template>
-
-<script lang="ts">
-import { defineComponent, ref } from 'vue';
-import useSpeech from '@/hooks/useSpeech';
-
-export default defineComponent({
-  name: 'Index',
-  setup() {
-    const { voices, options, say } = useSpeech();
-
-    // const essay = ref<string>('');
-    // const selVoice = ref<SpeechSynthesisVoice>(options.voice);
-    // const selVolume = ref<number>(options.volume);
-    // const selRate = ref<number>(options.rate);
-
-    function read() {
-      // options.text = essay.value;
-      // options.voice = selVoice.value;
-      // options.volume = selVolume.value;
-      // options.rate = selRate.value;
-      say();
-    }
-
-    return {
-      voices,
-      options,
-      // essay,
-      // selVoice,
-      // selVolume,
-      // selRate,
-      read
-    };
-  }
-});
-</script>
